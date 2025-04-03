199ef7dbe1bcb1ee0d52db5974197f367785f9f9
Kuba <kuba.pomor@gmail.com>
2025-04-03 08:24:33 +0200

    Add Sarrus method to lab-04

--- lab-04/src/main.rs
+++ lab-04/src/main.rs
@@ -35,12 +35,63 @@ fn det(matrix: &Matrix) -> f64 {
     match get_square_matrix_size(matrix) {
         1 => matrix[0][0],
         2 => (matrix[0][0] * matrix[1][1]) - (matrix[1][0] * matrix[0][1]),
+        3 => {
+            let n = get_square_matrix_size(matrix);
+            let mut sarrus_matrix = vec![vec![0.0; n + 2]; n];
+
+            for i in 0..n {
+                for j in 0..n {
+                    sarrus_matrix[i][j] = matrix[i][j];
+                }
+            }
+
+            for i in 0..n {
+                for j in 0..2 {
+                    sarrus_matrix[i][n + j] = matrix[i][j];
+                }
+            }
+
+            let mut det = 0.0;
+            let mut offset = 0;
+
+            for _ in 0..3 {
+                let mut d = 1.0;
+
+                for j in 0..n {
+                    // println!("{}:{}", j, j + offset);
+                    d *= sarrus_matrix[j][j + offset];
+                }
+
+                // println!("d = {}", d);
+
+                det += d;
+                offset += 1;
+            }
+
+            let mut offset = 0;
+
+            for _ in 0..3 {
+                let mut d = 1.0;
+
+                for j in (0..n).rev() {
+                    // println!("{}:{}", (n - 1) - j, j + offset);
+                    d *= sarrus_matrix[(n - 1) - j][j + offset];
+                }
+
+                // println!("d = {}", d);
+
+                det -= d;
+                offset += 1;
+            }
+
+            det
+        }
         n => {
             let mut det = 0.0;
 
             for j in 0..n {
-                let minor = get_minor(matrix, 0, j);
-                det += (-1.0_f64).powf(j as f64) * matrix[0][j] * self::det(&minor);
+                let c = get_minor(matrix, 0, j);
+                det += (-1.0_f64).powf(j as f64) * matrix[0][j] * self::det(&c);
             }
 
             det
@@ -180,11 +231,21 @@ fn print(matrix: &Matrix) {
     }
 }
 
+fn print_round(matrix: &Matrix) {
+    for row in matrix {
+        println!("{:?}", row.iter().map(|x| x.round().abs()).collect::<Vec<f64>>());
+    }
+}
+
 fn main() {
+    println!("Zadanie 1:");
+
     let square_matrix = vec![
         vec![2.0, 4.0],
         vec![1.0, 3.0]
     ];
+
+    print(&square_matrix);
 
     let det = det(&square_matrix);
     println!("{}", det);
@@ -195,8 +256,31 @@ fn main() {
         vec![1.0, 2.0, 3.0]
     ];
 
+    print(&big_square_matrix);
+
     let det = crate::det(&big_square_matrix);
     println!("{}", det);
+
+    let przyklad = vec![
+        vec![15.0, 11.0, 10.0, 15.0],
+        vec![19.0, 9.0, 0.0, 17.0],
+        vec![6.0, 9.0, 6.0, 13.0],
+        vec![0.0, 12.0, 12.0, 11.0]
+    ];
+
+    print(&przyklad);
+
+    let det = crate::det(&przyklad);
+    println!("{}", det);
+
+    println!();
+
+    println!("Zadanie 2:");
+
+    let transposed = transpose(&square_matrix);
+    print(&transposed);
+
+    println!();
 
     let matrix = vec![
         vec![1.0, 2.0, 3.0],
@@ -206,9 +290,43 @@ fn main() {
     let transposed = transpose(&matrix);
     print(&transposed);
 
+    println!();
+
+    println!("Zadanie 3:");
+
+    let inv = invert_gauss_jordan(&przyklad).unwrap();
+    print(&inv);
+
+    let det = crate::det(&inv);
+    println!("{}", det);
+
+    println!();
+
+    let aaa = multiply(&przyklad, &inv);
+    print(&aaa);
+
+    println!();
+
+    let aaa = multiply(&inv, &przyklad);
+    print(&aaa);
+
+    println!();
+
     let inverse = invert_laplacian(&big_square_matrix).unwrap();
     print(&inverse);
 
+    println!();
+
     let inverse = invert_gauss_jordan(&big_square_matrix).unwrap();
+    print(&inverse);
+
+    println!("==================");
+
+    let inverse = invert_laplacian(&przyklad).unwrap();
+    print(&inverse);
+
+    println!();
+
+    let inverse = invert_gauss_jordan(&przyklad).unwrap();
     print(&inverse);
 }
